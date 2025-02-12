92a6bc5f3d205c0bd5ee750721bcb0bb472f4d49
Harshal Khamkar <83208368+Harshal1810@users.noreply.github.com>
2025-02-12 17:41:19 +0530

    Updated prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,4 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+async function testConnection() {
+  try {
+    await prisma.$connect();
+    console.log("✅ Prisma connected to MongoDB");
+  } catch (error) {
+    console.error("❌ Prisma connection error:", error);
+  }
+}
+
+testConnection();
+
 export default prisma;
